c41d23d5a9e4887a159f2ef6d850d2946900401e
tonmoy <tonmoydeb404@gmail.com>
2021-10-16 23:19:00 +0600

    OG markup added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Search = styled('form')(({ theme }) => ({
         backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '100%',
+    width: '50%',
     [theme.breakpoints.up('sm')]: {
         marginLeft: theme.spacing(1),
         width: 'auto',
